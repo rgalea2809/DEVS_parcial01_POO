e7c5a84bdf88e013752fde698dc9802c0d2fa508
Rodrigo Mejia <00037119@uca.edu.sv>
2020-04-17 11:59:25 -0600

    Modify: changes in methods of CalculadoraImpuestos class

--- ProyectoParcial/src/com/REMG/x00037119/CalculadoraImpuestos.java
+++ ProyectoParcial/src/com/REMG/x00037119/CalculadoraImpuestos.java
@@ -1,9 +1,9 @@
 package com.REMG.x00037119;
 
 public class CalculadoraImpuestos {
-    private double totalRenta;
-    private double totalISSS;
-    private double totalAFP;
+    private static double totalRenta;
+    private static double totalISSS;
+    private static double totalAFP;
 
     private CalculadoraImpuestos(){ }
 
@@ -11,25 +11,38 @@ public class CalculadoraImpuestos {
         double total=0;
         double salario= empleado.getSalario();
 
-        if(empleado instanceof ServicioProfesional){
+         if(empleado instanceof ServicioProfesional){
             double renta1 = (salario * 0.1);
+             totalRenta += renta1;
             total= (salario - renta1);
-        }
-        else if(empleado instanceof PlazaFIja){
+            }
+         else if(empleado instanceof PlazaFIja) {
+            double renta2 = 0;
             double AFP = (0.0625 * salario);
             double ISSS = (0.03 * salario);
             double restante = (salario - AFP - ISSS);
 
+            if (restante >= 0.01 && restante <= 472.00) {
+                renta2 = 0;
+            } else if (restante >= 472.01 && restante <= 895.24) {
+                renta2 = (0.1 * (restante - 472) + 17.67);
+            } else if (restante >= 895.25 && restante <= 2037.10) {
+                renta2 = (0.2 * (restante - 895.24) + 60);
+            } else if (restante >= 2038.11) {
+                renta2 = (0.3 * (restante - 2038.10) + 288.57);
+            }
+            totalRenta+= renta2;
+            totalAFP += AFP;
+            totalISSS += ISSS;
 
-
-
+            total= (restante - renta2);
         }
         return total;
     }
 
     public static String mostrarTotales (){
 
-        return "asd";
+        return "TOTALES ACUMULADOS: \na) AFP: $"+ totalAFP+ ". \nb) ISS: $ "+totalISSS+ ". \nc)Renta: $"+totalRenta+".";
     }
 
 
